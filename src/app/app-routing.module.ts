981938e7dade20106d2b2ea8e42cf5bdb3317976
jimena <jimenahernando@hotmail.com>
2021-08-21 17:19:27 +0200

    Creo mapa de rutas,con raiz y de fallo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { InfoComponent } from './components/info/info.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // RUTA RAIZ
+  // { path: "" , component: HomeComponent },
+  { path: '' , pathMatch: 'full', redirectTo: '/home'},
+
+  { path: 'home' , component: HomeComponent },
+  { path: 'info' , component: InfoComponent },
+  { path: 'projects' , component: ProjectsComponent },
+  { path: 'about' , component: AboutComponent },
+
+  // RUTA DE FALLO
+  // { path: '**' , component: 404Component },
+  { path: '**' , pathMatch: 'full', redirectTo: '/home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
